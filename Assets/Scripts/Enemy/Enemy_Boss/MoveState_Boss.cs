77448b0ac19815448bf3ebc24181a5c4c9e146ce
truongk23k <truongk23k@gmail.com>
2026-04-28 21:45:25 +0700

    Boss balance

--- Assets/Scripts/Enemy/Enemy_Boss/MoveState_Boss.cs
+++ Assets/Scripts/Enemy/Enemy_Boss/MoveState_Boss.cs
@@ -6,7 +6,7 @@ public class MoveState_Boss : EnemyState
     private Vector3 destination;
 
     private float actionTimer;
-    private float timeBeforeSpeedUp = 10;
+    private float timeBeforeSpeedUp = 5;
 
     private bool speedUpActivated;
 
@@ -33,6 +33,7 @@ public class MoveState_Boss : EnemyState
         speedUpActivated = false;
 
         enemy.anim.SetFloat("MoveAnimIndex", 0);
+        enemy.anim.SetFloat("MoveAnimSpeedMultiplier", 1);
         enemy.agent.speed = enemy.walkSpeed;
     }
 
@@ -69,6 +70,7 @@ public class MoveState_Boss : EnemyState
     {
         enemy.agent.speed = enemy.runSpeed;
         enemy.anim.SetFloat("MoveAnimIndex", 1);
+        enemy.anim.SetFloat("MoveAnimSpeedMultiplier", 1.5f);
         speedUpActivated = true;
     }
 
